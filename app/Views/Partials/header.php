de0d65b2e8d15cd0bb388493d855797105ef76db
Leonardo Palacios Gonzales <leonardoalexander130@gmail.com>
2026-03-27 20:31:07 -0500

    Actualizado

--- app/Views/Partials/header.php
+++ app/Views/Partials/header.php
@@ -69,7 +69,6 @@
                         <a class="collapse-item" href="<?= base_url('clientes') ?>">Clientes</a>
                         <a class="collapse-item" href="<?= base_url('proveedores') ?>">Proveedores</a>
                         <a class="collapse-item" href="<?= base_url('productos') ?>">Productos</a>
-                        <a class="collapse-item" href="<?= base_url('vehiculos') ?>">Vehiculos</a>
                     </div>
                 </div>
             </li>
